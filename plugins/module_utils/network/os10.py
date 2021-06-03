c3dc03b804f794a232fde0f3e767a10236715517
Sean M. Collins <sean@coreitpro.com>
2021-06-03 12:19:35 -0400

    [WIP] Wrap commands in a transaction

--- plugins/module_utils/network/os10.py
+++ plugins/module_utils/network/os10.py
@@ -113,7 +113,7 @@ def load_config(module, commands):
     rc, out, err = exec_command(module, 'configure terminal')
     if rc != 0:
         module.fail_json(msg='unable to enter configuration mode', err=to_text(err, errors='surrogate_or_strict'))
-
+    commands.insert(0, 'start transaction')
     commands.append('commit')
     for command in to_list(commands):
         if command == 'end':
@@ -121,6 +121,7 @@ def load_config(module, commands):
         rc, out, err = exec_command(module, command)
         if rc != 0:
             module.fail_json(msg=to_text(err, errors='surrogate_or_strict'), command=command, rc=rc)
+            exec_command(module, 'discard')
 
     exec_command(module, 'end')
 
