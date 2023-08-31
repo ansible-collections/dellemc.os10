af5de531bed94d750b04f26ed60502eed16a7896
prasada.reddy <prasada.reddy@dellteam.com>
2023-08-31 05:25:30 +0000

    changed big line

--- plugins/module_utils/network/os10.py
+++ plugins/module_utils/network/os10.py
@@ -32,7 +32,7 @@
 
 from __future__ import (absolute_import, division, print_function)
 
-# import re
+import re
 
 from ansible.module_utils._text import to_text
 from ansible.module_utils.basic import env_fallback
