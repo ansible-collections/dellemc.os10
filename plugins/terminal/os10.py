310fcddaf07b65f88eff9d70f444413bf9509cd8
prasada.reddy <prasada.reddy@dellteam.com>
2023-11-09 06:16:30 +0000

    fix for ansible issue-152

--- plugins/terminal/os10.py
+++ plugins/terminal/os10.py
@@ -43,7 +43,7 @@ class TerminalModule(TerminalBase):
         re.compile(br"invalid input", re.I),
         re.compile(br"(?:incomplete|ambiguous) command", re.I),
         re.compile(br"connection timed out", re.I),
-        re.compile(br"[^\r\n]+ not found", re.I),
+        re.compile(br"% Error(.+?)not found", re.I),
         re.compile(br"'[^']' +returned error code: ?\d+"),
     ]
 
